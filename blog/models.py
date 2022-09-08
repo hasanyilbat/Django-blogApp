6e79b724576fdaac2fe061246f792636a3249881
Hasan <hasan.yilbat@gmail.com>
2022-09-08 23:28:50 +0300

    starter v2

--- blog/models.py
+++ blog/models.py
@@ -17,22 +17,3 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}" 
-from django.db import models
-
-# Create your models here.
-
-
-class Post(models.Model):
-	# id = models.IntegerField(unique=True)
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200)
-    content = models.TextField(max_length=250)
-    publish_date = models.DateTimeField(auto_now_add=True)
-    # last_update = models.DateTimeField(auto_now=True)
-    # image = 
-    # status = 
-	
-
-
-    def __str__(self):
-        return f"{self.title}" 
